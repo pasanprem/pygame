8238240c3741673e778184d0ff3245538ecedaf4
Pasan Premaratne <pasan@pasandev.com>
2019-01-25 14:13:05 +0000

    2nd

--- pygamestart.py
+++ pygamestart.py
@@ -4,16 +4,31 @@ black = (0,0,0)
 white = (255,255,255)
 
 pygame.init()
-surface = pygame.display.set_mode((800,400))
+
+surfaceWidth = 800
+surfaceHeight = 500
+
+surface = pygame.display.set_mode((surfaceWidth,surfaceHeight))
 pygame.display.set_caption('Helicopter')
 clock = pygame.time.Clock()
-
-def helicopter(x, y, image):
-    surface.blit(img, (x,y))
 
 img = pygame.image.load('Helicopter.png')
 x = 150
 y = 200
+
+def msgSurface(text):
+    smallText = pygame.font.Font('freesansbold.ttf', 20)
+    largeText = pygame.font.Font('freesansbold.ttf, 150')
+
+
+
+def gameOver():
+    msgSurface('Kaboom!')
+
+def helicopter(x, y, image):
+    surface.blit(img, (x,y))
+
+y_move = 0
 
 game_over = False
 
@@ -21,13 +36,25 @@ while not game_over:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_over = TRUE
+            game_over = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                y_move = -5
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                y_move = 5
+
+    y += y_move
 
     surface.fill(black)
     helicopter(x, y, img)
 
-    pygame.display.update()
+    if y > surfaceHeight-40 or y < 0:
+        gameOver()
 
+    pygame.display.update()
     clock.tick(60)
 
 pygame.quit()
